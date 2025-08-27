f9c9a1daba8c0c8fd82799ba7b260ec38f1a4a97
Peppereli <lastf8474@gmail.com>
2025-08-27 08:04:00 +0500

    added nvim config

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+require("plugins") 
+-- Now set global options
+vim.opt.clipboard = "unnamedplus"
+
+-- Other settings...
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
